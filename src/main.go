3d5fb610b20e6e72abae38731bb631bbefb6137a
abdulrazzak.almohamad <abdulrazzak.almohamad@gpay.com.tr>
2022-07-15 23:18:28 +0300

    added

--- src/main.go
+++ src/main.go
@@ -19,30 +19,8 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"greet": "hello, world!",
 		})
-	})
-  
-	router.GET("/products", GetProducts)
+	}) 
 
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
- 
-func GetProducts(c *gin.Context) {
-
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-	db.AutoMigrate(&Product{})
-
-	var products []Product
-
-	if err := db.Find(&products).Error; err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		log.Println(err)
-	} else {
-		c.JSON(http.StatusOK, products)
-		log.Println(products)
-	}
-
-}
+  
