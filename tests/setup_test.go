1a46dfd75cfb93b0d65b92f7363dbe474338c339
abdulrazzak.almohamad <abdulrazzak.almohamad@gpay.com.tr>
2022-07-17 22:08:45 +0300

    tests have added

---
+++ tests/setup_test.go
@@ -0,0 +1,108 @@
+package tests
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"github.com/abdalrazzak/gin-golang-test/api/controllers"
+	"github.com/abdalrazzak/gin-golang-test/api/models"
+)
+
+var server = controllers.Server{}
+var userInstance = models.User{} 
+
+func TestMain(m *testing.M) {
+	var err error
+	err = godotenv.Load(os.ExpandEnv("./../.env"))
+	if err != nil {
+		log.Fatalf("Error getting env %v\n", err)
+	}
+	Database()
+
+	os.Exit(m.Run())
+
+}
+
+func Database() {
+
+	var err error
+
+	TestDbDriver := os.Getenv("TEST_DB_DRIVER")
+
+	if TestDbDriver == "mysql" {
+		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("TEST_DB_USER"), os.Getenv("TEST_DB_PASSWORD"), os.Getenv("TEST_DB_HOST"), os.Getenv("TEST_DB_PORT"), os.Getenv("TEST_DB_NAME"))
+		server.DB, err = gorm.Open(TestDbDriver, DBURL)
+		if err != nil {
+			fmt.Printf("Cannot connect to %s database\n", TestDbDriver)
+			log.Fatal("This is the error:", err)
+		} else {
+			fmt.Printf("We are connected to the %s database\n", TestDbDriver)
+		}
+	} 
+}
+
+func refreshUserTable() error {
+	err := server.DB.DropTableIfExists(&models.User{}).Error
+	if err != nil {
+		return err
+	}
+	err = server.DB.AutoMigrate(&models.User{}).Error
+	if err != nil {
+		return err
+	}
+	log.Printf("Successfully refreshed table")
+	return nil
+}
+
+func seedOneUser() (models.User, error) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := models.User{
+		Age:	   22,
+		Email:    "abboudbath4@gmail.com",
+		Password: "password",
+	}
+
+	err = server.DB.Model(&models.User{}).Create(&user).Error
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+func seedUsers() ([]models.User, error) {
+
+	var err error
+	if err != nil {
+		return nil, err
+	}
+	users := []models.User{
+		models.User{
+			Age: 	   22,
+			Email:    "abboudbath4@gmail.com",
+			Password: "password",
+		},
+		models.User{
+			Age: 	   22,
+			Email:    "abboudbath4@gmail.com",
+			Password: "password",
+		},
+	}
+
+	for i, _ := range users {
+		err := server.DB.Model(&models.User{}).Create(&users[i]).Error
+		if err != nil {
+			return []models.User{}, err
+		}
+	}
+	return users, nil
+}
+ 
